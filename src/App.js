d5aa326dc1c4880150548242b8bff34c65933b14
Kasia Janiszewska <katarzyna.janiszewska@gmail.com>
2020-08-12 23:14:59 +0200

    app basic structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NewsList from './components/NewsList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          hello world!
+        </div>
+      </header>
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewsList.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react'
+// import { makeStyles } from '@material-ui/core/styles';
+// import Accordion from '@material-ui/core/Accordion';
+// import AccordionSummary from '@material-ui/core/AccordionSummary';
+// import AccordionDetails from '@material-ui/core/AccordionDetails';
+// import Typography from '@material-ui/core/Typography';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: '100%',
+//   },
+//   heading: {
+//     fontSize: theme.typography.pxToRem(15),
+//     fontWeight: theme.typography.fontWeightRegular,
+//   },
+// }));
+
+// export default function SimpleAccordion() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <Accordion>
+//         <AccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//           aria-controls="panel1a-content"
+//           id="panel1a-header"
+//         >
+//           <Typography className={classes.heading}>Accordion 1</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//       <Accordion>
+//         <AccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//           aria-controls="panel2a-content"
+//           id="panel2a-header"
+//         >
+//           <Typography className={classes.heading}>Accordion 2</Typography>
+//         </AccordionSummary>
+//         <AccordionDetails>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+//             sit amet blandit leo lobortis eget.
+//           </Typography>
+//         </AccordionDetails>
+//       </Accordion>
+//       <Accordion disabled>
+//         <AccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//           aria-controls="panel3a-content"
+//           id="panel3a-header"
+//         >
+//           <Typography className={classes.heading}>Disabled Accordion</Typography>
+//         </AccordionSummary>
+//       </Accordion>
+//     </div>
+//   );
+// }
+
+const NewsList = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const fetchNews = () => {
+    fetch("https://newscatcher.p.rapidapi.com/v1/stocks?media=True&lang=en&ticker=AAPL", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "newscatcher.p.rapidapi.com",
+        "x-rapidapi-key": `${process.env.REACT_APP_BUTTER_API_TOKEN}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .then(data => setNews(data))
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div>
+      {/* {news} */}
+    </div>
+  );
+};
+
+export default NewsList;
