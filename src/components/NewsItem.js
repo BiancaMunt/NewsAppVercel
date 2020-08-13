ae9be99f1ea19cccc3d931adf863afb8520f3a91
Kasia Janiszewska <katarzyna.janiszewska@gmail.com>
2020-08-13 12:31:05 +0200

    displaying articles fetched from external API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,27 +4,31 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import PropTypes from 'prop-types';
 
-const NewsItem = ({ data, index, classes }) => {
-  console.log(data.summary);
-  return (
-    <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls={`panel${index}a-content`}
-          id={`panel${index}a-header`}
-        >
-          <Typography className={classes.heading}>{data.title}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            {data.summary}
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </div>
-  );
-};
+const NewsItem = ({ data, index, classes }) => (
+  <div>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls={`panel${index}a-content`}
+        id={`panel${index}a-header`}
+      >
+        <Typography className={classes.heading}>{data.title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          {data.summary}
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  </div>
+);
 
 export default NewsItem;
+
+NewsItem.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+  index: PropTypes.number.isRequired,
+  classes: PropTypes.objectOf(PropTypes.any).isRequired,
+};
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,11 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import NewsItem from './NewsItem';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import Accordion from '@material-ui/core/Accordion';
-// import AccordionSummary from '@material-ui/core/AccordionSummary';
-// import AccordionDetails from '@material-ui/core/AccordionDetails';
-// import Typography from '@material-ui/core/Typography';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import NewsItem from './NewsItem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,78 +12,29 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// export default function SimpleAccordion() {
-
-//   return (
-//     <div className={classes.root}>
-//       <Accordion>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel1a-content"
-//           id="panel1a-header"
-//         >
-//           <Typography className={classes.heading}>Accordion 1</Typography>
-//         </AccordionSummary>
-//         <AccordionDetails>
-//           <Typography>
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-//             sit amet blandit leo lobortis eget.
-//           </Typography>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel2a-content"
-//           id="panel2a-header"
-//         >
-//           <Typography className={classes.heading}>Accordion 2</Typography>
-//         </AccordionSummary>
-//         <AccordionDetails>
-//           <Typography>
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-//             sit amet blandit leo lobortis eget.
-//           </Typography>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion disabled>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel3a-content"
-//           id="panel3a-header"
-//         >
-//           <Typography className={classes.heading}>Disabled Accordion</Typography>
-//         </AccordionSummary>
-//       </Accordion>
-//     </div>
-//   );
-// }
-
 const NewsList = () => {
   const classes = useStyles();
   const [news, setNews] = useState();
+
+  const fetchNews = () => {
+    fetch('https://newscatcher.p.rapidapi.com/v1/stocks?&page=3&media=True&lang=en&ticker=AAPL', {
+      headers: {
+        'x-rapidapi-host': 'newscatcher.p.rapidapi.com',
+        'x-rapidapi-key': `${process.env.REACT_APP_BUTTER_API_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setNews(data))
+      .catch((err) => err);
+  };
 
   useEffect(() => {
     fetchNews();
   }, []);
 
-  const fetchNews = () => {
-    fetch("https://newscatcher.p.rapidapi.com/v1/stocks?media=True&lang=en&ticker=AAPL", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "newscatcher.p.rapidapi.com",
-        "x-rapidapi-key": `${process.env.REACT_APP_BUTTER_API_TOKEN}`
-      }
-    })
-      .then(res => res.json())
-      // .then(data => console.log(data))
-      .then(data => setNews(data))
-      .catch(err => console.log(err));
-  }
-
   return (
     <div className={classes.root}>
-      {news ? news.articles.map((article, i) => <NewsItem key={article._id} data={article} index={i + 1} classes={classes} />) : 'LOADING!!' }
+      {news ? news.articles.map((article, i) => <NewsItem key={article._id} data={article} index={i + 1} classes={classes} />) : 'LOADING!!'}
     </div>
   );
 };
